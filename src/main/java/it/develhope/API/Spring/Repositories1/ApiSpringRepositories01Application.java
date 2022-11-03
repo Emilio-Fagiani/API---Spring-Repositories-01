e2ce2cefc8f6deba3a930bfbb4489b599e0f2338
Emilio <emi00@gmail.com>
2022-11-03 17:16:48 +0100

    First commit

---
+++ src/main/java/it/develhope/API/Spring/Repositories1/ApiSpringRepositories01Application.java
@@ -0,0 +1,13 @@
+package it.develhope.API.Spring.Repositories1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringRepositories01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringRepositories01Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/develhope/API/Spring/Repositories1/ApiSpringRepositories01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.API.Spring.Repositories1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringRepositories01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
